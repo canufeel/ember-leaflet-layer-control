f21dddbb55495c98c8819dbd1b119d5c68c68e2b
Ashley Sommer <Ashley.Sommer@csiro.au>
2016-11-29 09:12:07 +1000

    Update ember-leaflet dependency to 3.0.5, Fix issue with `ember-cli-htmlbars` in `devDependecies` vs `dependencies`. It is now in both, which issues a warning, but works correctly. Fix a broken test. Fix old variable naming.

--- addon/components/layer-control.js
+++ addon/components/layer-control.js
@@ -12,29 +12,30 @@ export default BaseLayer.extend({
     }
     this.didCreateLayer();
   },
-  createLayer(){
-    return L.control.layers();
+  createLayer() {
+    //L is defined globally by Leaflet
+    return L.control.layers(); // jshint ignore:line
   },
   didCreateLayer() {
     this._super(...arguments);
     var layerGroups = get(this,'parentComponent._layerGroups');
     if (!isBlank(layerGroups)){
-      layerGroups.forEach(layerGroup=>{
+      layerGroups.forEach(layerGroup => {
         if (layerGroup.default){
-          let containerLayer = get(this,'parentComponent._layer');
-          layerGroup.layer.addTo(containerLayer);
+          let parentLayer = get(this,'parentComponent._layer');
+          layerGroup.layer.addTo(parentLayer);
         }
-        this._layer.addOverlay(layerGroup.layer,layerGroup.name);
+        this._layer.addOverlay(layerGroup.layer, layerGroup.name);
       });
     }
     let baseLayers = get(this,'parentComponent._baseLayers');
     if (!isBlank(baseLayers)){
       baseLayers.forEach(baseLayer=>{
         if (baseLayer.default){
-          let containerLayer = get(this,'parentComponent._layer');
-          baseLayer.layer.addTo(containerLayer);
+          let parentLayer = get(this,'parentComponent._layer');
+          baseLayer.layer.addTo(parentLayer);
         }
-        this._layer.addBaseLayer(baseLayer.layer,baseLayer.name);
+        this._layer.addBaseLayer(baseLayer.layer, baseLayer.name);
       });
     }
 
